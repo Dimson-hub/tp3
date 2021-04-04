eae48935b55f7491a5ea143b91fa7a2352564e62
DIMA Adamou <adamou.dima@etud.iga.ac.ma>
2021-04-04 12:37:50 +0100

    initial commit

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import bar from './bar.js.js';
+const Index = () => {
+  return <div>TIW 8 TP1!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
+bar();
